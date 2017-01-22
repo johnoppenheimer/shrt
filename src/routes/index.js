5f268900d1a1ee728af3a16e59ae9b2df33648f7
Maxime Cattet <maximecattet@gmx.com>
2017-01-22 13:04:59 +0100

    :sparkles: Add redirection
    In case someone run Shrt only with the API

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import httpStatus from "http-status";
 
 import healthRoutes from "./health";
 import linkRoutes from "./links";
+import Link from "../models/link";
+import APIError from "../helpers/api-error";
 
 const router = express.Router();
 
@@ -9,6 +12,19 @@ router.get("/", (req, res) => {
     res.json({
         "message": req.__("Shrt API")
     });
+});
+
+router.get("/:linkId", (req, res, next) => {
+    Link.get(req.params.linkId)
+        .then(link => {
+            if (link) {
+                res.redirect(link.url);
+            }
+            else {
+                next(new APIError(["This link does not exist"], httpStatus.NOT_FOUND));
+            }
+        })
+        .catch(e => next(e));
 });
 
 router.use("/health-check", healthRoutes);
